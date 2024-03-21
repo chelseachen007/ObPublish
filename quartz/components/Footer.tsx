817e1fac020d8d4474235d793eafda3844e8aa5e
chelsechen007 <aqwejie@qq.com>
2024-03-21 12:50:26 +0800

    fix: bug

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -2,6 +2,7 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
+import Comments from "./Comments"
 
 interface Options {
   links: Record<string, string>
@@ -12,20 +13,23 @@ export default ((opts?: Options) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
-      <footer class={`${displayClass ?? ""}`}>
-        <hr />
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
-      </footer>
+      <div>
+        {Comments()()}
+        <footer class={`${displayClass ?? ""}`}>
+          <hr />
+          <p>
+            {i18n(cfg.locale).components.footer.createdWith}{" "}
+            <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          </p>
+          <ul>
+            {Object.entries(links).map(([text, link]) => (
+              <li>
+                <a href={link}>{text}</a>
+              </li>
+            ))}
+          </ul>
+        </footer>
+      </div>
     )
   }
 
